b75616de81f8ca7a7cfe2e6de1a416653b30060d
EnricMS <enricmiralles96@gmail.com>
2023-04-26 23:43:53 +0200

    actualitzacio 26/04 enllaços + altres

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function guardarEmail() {
     let inputEmail = document.getElementById("email").value;
     if (inputEmail !== "") {
         emails.push(inputEmail);
-        document.getElementById("emailsGuardados").innerHTML = "Emails guardados: " + emails.join(", ");
+        document.getElementById("SavedEmails").innerHTML = "SavedEmails: " + emails.join(", ");
     }
 }
 
